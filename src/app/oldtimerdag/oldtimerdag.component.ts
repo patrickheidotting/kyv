0b324016f1df67dd46724dbdd0de040bf3465909
Patrick Heidotting <patrick@heidotting.nl>
2025-05-04 08:57:35 +0200

    pagina oldtimerdag aangevuld

--- src/app/oldtimerdag/oldtimerdag.component.ts
+++ src/app/oldtimerdag/oldtimerdag.component.ts
@@ -1,8 +1,11 @@
 import {Component} from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-oldtimerdag',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './oldtimerdag.component.html',
   styleUrl: './oldtimerdag.component.css'
 })
